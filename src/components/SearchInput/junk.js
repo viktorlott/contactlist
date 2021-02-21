05cc01de34a2cd624d6bea0523a4be009efb9e4f
Viktor Lott <viktor.lott@selfpoint.se>
2021-02-21 20:38:04 +0100

    wip

--- src/components/SearchInput/junk.js
+++ src/components/SearchInput/junk.js
@@ -1,4 +1,44 @@
+const UserListContainer = styled.div`
+    position: absolute;
+    top: calc(100% + 10px);
+    display: ${props => props.hide ? "none" : "flex"};
+    flex-direction: column;
+    width: 100%;
+    border-radius: 8px;
+    box-shadow: 0px 8px 20px rgb(0 0 0 / 6%);
+    background: white;
+    color: #8a8d90;
+    overflow: scroll;
+    cursor: pointer;
+    z-index: 2;
+    
 
+    max-height: 430px;
+    
+    >p {
+        
+        color: #767a84;
+        color: #a2a5ad;
+        font-weight: 300;
+        transition: transform 0.1s ease-in, background 0.1s ease-in, color 0.1s ease-in ;
+        &:hover {
+            transform: scale(1.01);
+            color:#626569;
+            background: #f4f4f4;
+            
+        }
+        position: relative;
+        padding: 15px 30px;
+        margin: 5px 0;
+        >svg {
+            position: absolute;
+            right: 20px;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    }
+
+`
 /**
  * 
  * This is my first interation of the search list. But i decided to scap this and go with contact cards instead.
